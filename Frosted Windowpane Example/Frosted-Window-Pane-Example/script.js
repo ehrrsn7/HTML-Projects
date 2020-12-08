232f4281976de6af49da398326896c7f81219f7d
E Harrison <65516014+ehrrsn7@users.noreply.github.com>
2020-12-08 10:31:02 -0800

    Estudos project
    implemented frosted windowpane div effect and parallax effect

---
+++ Frosted Windowpane Example/Frosted-Window-Pane-Example/script.js
@@ -0,0 +1,6 @@
+// prevent side-side scrolling
+// TODO: add this functionality
+
+// DARK MODE
+let prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+let darkMode = prefersDarkScheme.matches;
